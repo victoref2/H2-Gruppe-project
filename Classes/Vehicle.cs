360c1fe7ada4b132a7757292172f3bd68854a6a8
Casper Simon Jensen <casp142b@elev.techcollege.dk>
2024-09-04 13:20:01 +0200

    Classes folder

---
+++ Classes/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H2_Gruppe_project.Classes
+{
+    internal class Vehicle
+    {
+        public string Id { get; private set; }
+        public string Name { get; private set; }
+        public string KM { get; private set; }
+        public string RegristrationNumber {  get; private set; }
+        public string AgeGroup { get; private set; }
+        public bool TowHook { get; set; }
+        public string Drives {  get; private set; }
+    }
+}
