47bbfe4f8502430ac3b8d563010076b7d34fbd32
Casper Simon Jensen <casp142b@elev.techcollege.dk>
2024-09-04 13:23:56 +0200

    V1:Vehicle Class

--- Classes/Vehicle.cs
+++ Classes/Vehicle.cs
@@ -8,12 +8,17 @@ namespace H2_Gruppe_project.Classes
 {
     internal class Vehicle
     {
+        //change privacy and what it is as needed
         public string Id { get; private set; }
         public string Name { get; private set; }
         public string KM { get; private set; }
         public string RegristrationNumber {  get; private set; }
         public string AgeGroup { get; private set; }
         public bool TowHook { get; set; }
-        public string Drives {  get; private set; }
+        public string DrivesLicenceClass {  get; private set; }
+        public string EngienSize { get; private set; }
+        public string KmL {  get; private set; }
+        public string FuelType {  get; private set; }
+        public string EnergyClass {  get; private set; }
     }
 }
