1914a35edbf63affe3bbeedd804801a3728e5a40
Casper Simon Jensen <casp142b@elev.techcollege.dk>
2024-09-05 09:18:59 +0200

    made the .cs file for Database needs the code

---
+++ DatabaseClasses/Database.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H2_Gruppe_project.DatabaseClasses
+{
+    public partial class Database
+    {
+        public string ConnectionString { get; set; }
+
+        public static Database Instance { get; private set; } = new Database();
+    }
+}
---
+++ DatabaseClasses/NewUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H2_Gruppe_project.DatabaseClasses
+{
+    public partial class Database
+    {
+    }
+}
