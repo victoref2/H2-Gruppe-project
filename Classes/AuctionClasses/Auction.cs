22bca80433089e9da0f0abdc9daf910deaa5bca0
Casper Simon Jensen <casp142b@elev.techcollege.dk>
2024-09-05 10:08:11 +0200

    A1: Base auction class made

---
+++ Classes/AuctionClasses/Auction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H2_Gruppe_project.Classes;
+
+public class Auction
+{
+    public string Id {  get; private set; }
+    Vehicle vehicle {get; set; }
+    User user {get; set; }
+    public int Price {  get; set; }
+    public Auction(string id, Vehicle vehicle, User user,int price)
+    {
+        Id = id;
+        Price = price;
+    }
+}
