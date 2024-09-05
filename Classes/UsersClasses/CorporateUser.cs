c24b63a41746e8a7ef9b27814c7b20918edb8da3
Casper Simon Jensen <casp142b@elev.techcollege.dk>
2024-09-05 10:02:01 +0200

    U1,U3,U7,U9;made the base class User with the ToString for it made CorporateUser with a ToString for it

---
+++ Classes/UsersClasses/CorporateUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H2_Gruppe_project.Classes;
+
+public class CorporateUser:User
+{
+    public string Credit {  get; set; }
+    public string CVRNumber {  get; set; }
+    public CorporateUser(string id, string name, string passWord, string mailNumber, string credit, string cvrNumber)
+            : base(id, name, passWord, mailNumber)
+    {
+        Credit = credit;
+        CVRNumber = cvrNumber;
+    }
+
+    public override string ToString()
+    {
+        return base.ToString() + $", Credit: {Credit}, CVR Number: {CVRNumber}";
+    }
+}
---
+++ Classes/UsersClasses/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H2_Gruppe_project.Classes;
+
+public class User
+{
+    public string Id { get; private set; }
+    public string Name { get; set; }
+    public string PassWord { get; set; }
+    public string MailNumber { get; set; }
+
+    public User(string id,string name,string passWord,string mailNumber) 
+    { 
+        Id = id;
+        Name = name;
+        PassWord = passWord;
+        MailNumber = mailNumber;
+    }
+    public override string ToString() 
+    {
+        return $"User [ID: {Id}, Name: {Name}, Mail: {MailNumber}, Password: ****]";
+    }
+}
