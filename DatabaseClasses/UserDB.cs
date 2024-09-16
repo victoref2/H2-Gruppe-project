39f9d499a35dd4d4b4097cabb7f5659282bedfcb
Casper Simon Jensen <casp142b@elev.techcollege.dk>
2024-09-16 08:49:43 +0200

    explenation to what UserDB is going to be used for

--- DatabaseClasses/UserDB.cs
+++ DatabaseClasses/UserDB.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace H2_Gruppe_project.DatabaseClasses
 {
+    //this .cs is meant for the creation of users maybe in the future for the login as the user instead
+    //of the hard coded connection string
     public partial class Database
     {
     }
