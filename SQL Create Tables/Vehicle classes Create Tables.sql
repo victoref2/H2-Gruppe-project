f23af3dcde1e4662f3f9c5c68a388ed6086f5b7d
Casper Simon Jensen <casp142b@elev.techcollege.dk>
2024-09-19 13:23:58 +0200

    Attached My Vehicle Create Table To This new folder

---
+++ SQL Create Tables/Vehicle classes Create Tables.sql
@@ -0,0 +1,68 @@
+CREATE TABLE Vehicles (
+    VehicleId INT IDENTITY(1,1) PRIMARY KEY, 
+    Name VARCHAR(100) NOT NULL,
+    KM VARCHAR(20) NOT NULL,
+    RegistrationNumber VARCHAR(20) UNIQUE NOT NULL,
+    AgeGroup VARCHAR(20) NOT NULL,
+    TowHook BIT NOT NULL,
+    DriversLicenceClass VARCHAR(5) NOT NULL,
+    EngineSize DECIMAL NOT NULL,
+    KmL DECIMAL(10, 2) NOT NULL,
+    FuelType VARCHAR(20) NOT NULL,
+    EnergyClass VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE HeavyVehicles (
+    HeavyVehicleId INT IDENTITY(1,1) PRIMARY KEY,
+    VehicleId INT NOT NULL,
+    MaxLoadCapacity INT NOT NULL,
+    NumberOfAxles INT NOT NULL,
+    FOREIGN KEY (VehicleId) REFERENCES Vehicles(VehicleId) ON DELETE CASCADE
+);
+
+
+CREATE TABLE Trucks (
+    TruckId INT IDENTITY(1,1) PRIMARY KEY,
+    HeavyVehicleId INT NOT NULL,
+    Height DECIMAL(10, 2) NOT NULL,
+    Weight DECIMAL(10, 2) NOT NULL,
+    Length DECIMAL(10, 2) NOT NULL,
+    LoadCapacity DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (HeavyVehicleId) REFERENCES HeavyVehicles(HeavyVehicleId) ON DELETE CASCADE
+);
+
+CREATE TABLE Buses (
+    BusId INT IDENTITY(1,1) PRIMARY KEY,
+    HeavyVehicleId INT NOT NULL,
+    Height DECIMAL(10, 2) NOT NULL,
+    Weight DECIMAL(10, 2) NOT NULL,
+    Length DECIMAL(10, 2) NOT NULL,
+    NumberOfSeats INT NOT NULL,
+    NumberOfSleepingPlaces INT NOT NULL,
+    HasToilet BIT NOT NULL,
+    FOREIGN KEY (HeavyVehicleId) REFERENCES HeavyVehicles(HeavyVehicleId) ON DELETE CASCADE
+);
+
+CREATE TABLE NormalVehicles (
+    NormalVehicleId INT IDENTITY(1,1) PRIMARY KEY,
+    VehicleId INT NOT NULL,
+    NumberOfSeats INT NOT NULL,
+    TrunkDimensions VARCHAR(50) NOT NULL,
+    IsCommercial BIT NOT NULL,
+    FOREIGN KEY (VehicleId) REFERENCES Vehicles(VehicleId) ON DELETE CASCADE
+);
+
+CREATE TABLE PrivateVehicles (
+    PrivateVehicleId INT IDENTITY(1,1) PRIMARY KEY,
+    NormalVehicleId INT NOT NULL,
+    IsofixMount BIT NOT NULL,
+    FOREIGN KEY (NormalVehicleId) REFERENCES NormalVehicles(NormalVehicleId) ON DELETE CASCADE
+);
+
+CREATE TABLE CommercialVehicles (
+    CommercialVehicleId INT IDENTITY(1,1) PRIMARY KEY,
+    NormalVehicleId INT NOT NULL,
+    RollCage BIT NOT NULL,
+    LoadCapacity INT NOT NULL CHECK (LoadCapacity >= 0),
+    FOREIGN KEY (NormalVehicleId) REFERENCES NormalVehicles(NormalVehicleId) ON DELETE CASCADE
+);
