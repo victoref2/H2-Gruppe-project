e63fc228e1d3c40385769c1c1799f7ce679331b6
victoref2 <victoref3@gmail.com>
2024-10-01 12:29:48 +0200

    Error Fix maybe?

--- Classes/VehicleClasses/NormalVehicle.cs
+++ Classes/VehicleClasses/NormalVehicle.cs
@@ -1,24 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace H2_Gruppe_project.Classes
 {
     public class NormalVehicle : Vehicle
     {
-        public int NormalVehicleId {get; set; }
+        public int NormalVehicleId { get; set; }
         public int NumberOfSeats { get; private set; }
         public string TrunkDimensions { get; private set; } // e.g., "1.5m x 1m x 0.8m"
+        public bool IsCommercial { get; set; }  // Changed to follow naming conventions
 
         public NormalVehicle(string id, string name, string km, string regristrationNumber, string ageGroup, bool towHook,
             string driversLicenceClass, string engineSize, decimal kmL, string fuelType, string energyClass,
             int numberOfSeats, string trunkDimensions, bool isCommercial)
             : base(id, name, km, regristrationNumber, ageGroup, towHook, driversLicenceClass, engineSize, kmL, fuelType, energyClass)
         {
+            IsCommercial = isCommercial;
 
-            if (isCommercial && towHook)
+            if (IsCommercial && towHook)
             {
                 DriversLicenceClass = "BE";
             }
@@ -46,10 +44,10 @@ namespace H2_Gruppe_project.Classes
                 throw new ArgumentException("Invalid engine size format. It should be a decimal followed by 'L'.", nameof(engineSize));
             }
         }
+
         public override string ToString()
         {
             return base.ToString() + $", Number of Seats: {NumberOfSeats}, Trunk Dimensions: {TrunkDimensions}";
         }
     }
 }
-
