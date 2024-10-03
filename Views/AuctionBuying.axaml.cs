36021a8971d1435b823536ed63d519b08bf35979
victoref2 <victoref3@gmail.com>
2024-10-03 14:41:44 +0200

    Views were ignore for some reason??

---
+++ Views/AuctionBuying.axaml.cs
@@ -0,0 +1,20 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace H2_Gruppe_project.Views
+{
+
+    public partial class AuctionBuying : UserControl
+    {
+        public AuctionBuying()
+        {
+            InitializeComponent();
+        }
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+
+}
