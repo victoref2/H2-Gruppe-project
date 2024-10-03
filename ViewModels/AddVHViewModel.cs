035755bd08961162ae86bf2f53f454c269e3bbf7
Casper Simon Jensen <casp142b@elev.techcollege.dk>
2024-10-03 09:44:52 +0200

    Visual functionalty fix and "completion" just gotta move around some stuff and make the create auction button do something

--- ViewModels/AddVHViewModel.cs
+++ ViewModels/AddVHViewModel.cs
@@ -4,6 +4,9 @@ using System.Threading.Tasks;
 using H2_Gruppe_project.DatabaseClasses;
 using System;
 using H2_Gruppe_project.Classes;
+using Avalonia.Controls;
+using System.Collections.Generic;
+using Microsoft.SqlServer.Server;
 
 namespace H2_Gruppe_project.ViewModels
 {
@@ -32,6 +35,10 @@ namespace H2_Gruppe_project.ViewModels
         [ObservableProperty]
         private bool isHeavyVehicleVisible;
 
+        [ObservableProperty]
+        private bool isNormalVHVisible;
+
+
         // Command for going back to dashboard
         [RelayCommand]
         public void GoBackCancel()
@@ -39,15 +46,38 @@ namespace H2_Gruppe_project.ViewModels
             _mainViewModel.SwitchViewModel(new DashboardViewModel(_mainViewModel, _loggedInUser));
         }
 
-        // Method to handle ComboBox selection changes
+        [ObservableProperty]
+        private bool isCommercialVH;
+
+        // Visibility properties for private and commercial vehicles
+        [ObservableProperty]
+        private bool isCommercialVisible;
+
+        [ObservableProperty]
+        private bool isPrivateVisible;
+
+        partial void OnIsCommercialVHChanged(bool value)
+        {
+            // Set visibility based on the checkbox
+            IsCommercialVisible = value;
+            IsPrivateVisible = !value;
+        }
+
+
+        public List<string> VehicleTypes { get; } = new List<string> { "Truck", "Bus", "Normal Vehicle" };
+
         partial void OnSelectedVehicleTypeChanged(string value)
         {
             // Reset all visibilities
             IsTruckVisible = false;
             IsBusVisible = false;
             IsHeavyVehicleVisible = false;
+            IsNormalVHVisible = false;
 
-            // Set visibility based on the selected vehicle type
+            IsCommercialVisible = false;
+            IsCommercialVH = false;
+            IsPrivateVisible = false;
+
             if (value == "Truck")
             {
                 IsTruckVisible = true;
@@ -57,6 +87,12 @@ namespace H2_Gruppe_project.ViewModels
             {
                 IsHeavyVehicleVisible = true;
                 IsBusVisible = true;
+
+            }
+            else if (value == "Normal Vehicle")
+            {
+                IsNormalVHVisible = true;
+                IsPrivateVisible = true;
             }
         }
     }
