10aca3a303d27504d2e17a4f976e60dcc637b48b
victoref <victoref3@gmail.com>
2024-10-04 12:58:41 +0200

    converter made i guess

---
+++ DecimalToStringConverter.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace H2_Gruppe_project.Converters
+{
+    public class DecimalToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is decimal decimalValue)
+            {
+                return decimalValue.ToString(culture);
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string stringValue && decimal.TryParse(stringValue, out decimal result))
+            {
+                return result;
+            }
+            return 0m; // default decimal value if conversion fails
+        }
+    }
+}
