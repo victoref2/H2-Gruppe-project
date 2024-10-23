66ca92411f368249ae6493d59e2bdb3b1333e773
Casper Simon Jensen <casp142b@elev.techcollege.dk>
2024-10-23 10:32:16 +0200

    Final Commit functionality in HelpMeView and ViewModel(Bid History) now show all the bids you have won and every auction you have made that are over

---
+++ Views/HelpMeView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace H2_Gruppe_project;
+
+public partial class HelpMeView : UserControl
+{
+    public HelpMeView()
+    {
+        InitializeComponent();
+    }
+}
